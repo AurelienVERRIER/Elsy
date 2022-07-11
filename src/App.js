ed2612440ad78469f2568d4e6e40c1558804a9d2
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-11 17:30:39 +0200

    Exercice 3 almost done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import './App.css';
+import './styles/global.css';
+import Steps from './components/Steps'
 
 const tempsMin = -20
 const tempsMAx = 40
@@ -16,6 +18,7 @@ class App extends React.Component {
           <p>Heart : 80</p>
           <p>Temperature : -20</p>
           <p>Steps : 0</p>
+          <Steps />
         </div>
       </div>
     );
---
+++ src/components/Steps.js
@@ -0,0 +1,20 @@
+import React from "react";
+import '../App.css';
+
+class Steps extends React.Component {
+  render() {
+    return (
+      <div className="box col-sm3 col-6">
+        
+      
+          
+          <span className="material-icons" style={{fontSize:100, color:"red"}}>
+          directions_walk
+          </span>
+        </div>
+    );
+  }
+
+}
+
+export default Steps
