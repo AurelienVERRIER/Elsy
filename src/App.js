1c4e8033990dbaeb0d3c5f0004341088ec4e701c
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-07-13 17:58:58 +0200

    working on range features

--- src/App.js
+++ src/App.js
@@ -4,54 +4,64 @@ import './styles/global.css';
 import Box from './components/Box';
 
 const tempsMin = -20
-const tempsMAx = 40
+const tempsMax = 40
 const heartMin = 80
 const heartMax = 180
 const stepsMin = 0
 const stepsMax = 50000
 
 class App extends React.Component {
+  constructor () {
+  super()
+  this.state = {
+    water: 0,
+    heart: 120,
+    temperature: -10,
+    steps: 3000
+  }}
+
   render() {
     return (
       <div className="container-fluid">
         
         <div className="row">
-          <p>Heart : {heartMin}</p>
-          <p>Temperature : {tempsMin}</p>
-          <p>Steps : {stepsMin}</p>
           
-        {/* // Water Box*/}
-        <Box  
-          icon="local_drink" 
-          color="#3A85FF" 
-          value={1.5} 
-          unit=" L"
-        />
-          
-        {/* Steps Box */}
-        <Box 
-          icon="directions_walk"
-          color="black"
-          value ={3000}
-          unit = " steps"
-        />
-          
-        {/* Hearth Box */}
-        <Box 
-          icon="favorite"
-          color="red"
-          value ={120}
-          unit =" bpm"
-        />
-          
-          
-        {/* Temperature Box */}
-        <Box 
-          icon="wb_sunny"
-          color="yellow"
-          value ="-10"
-          unit ="°C"
-        />
+          <Box  
+            icon="local_drink" 
+            color="#3A85FF" 
+            value={this.state.water} 
+            unit=" L"
+          />
+
+          <Box 
+            icon="directions_walk"
+            color="black"
+            value ={this.state.steps}
+            unit = " steps"
+            rangeMin = {stepsMin}
+            rangeMax = {stepsMax}
+          />
+ 
+          <Box 
+            icon="favorite"
+            color="red"
+            value ={this.state.heart}
+            unit =" bpm"
+            rangeMin = {heartMin}
+            rangeMax = {heartMax}
+          />
+
+          <div>
+            <Box 
+              icon="wb_sunny"
+              color="yellow"
+              value ={this.state.temperature}
+              unit ="°C"
+              rangeMin = {tempsMin}
+              rangeMax = {tempsMax}
+            />
+            
+          </div>
 
         </div>
       </div>
--- src/components/Box.js
+++ src/components/Box.js
@@ -5,17 +5,22 @@ class Box extends React.Component {
   render() {
     return (
       <div className="box col-sm3 col-6">
-          <span className="material-icons" 
-          style={{
+          <span
+            className="material-icons" 
+            style={{
               fontSize: 100, 
               color: this.props.color
             }}>
             {this.props.icon}
           </span>
+
           <p>
             {this.props.value}
             {this.props.unit}
           </p>
+
+          <input type="range" min={this.props.rangeMin} max={this.props.rangeMax}/>
+
       </div>
     );
   }
